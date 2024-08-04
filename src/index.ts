58d6a777e6577aa93a90c98465ba977a8a23eacf
alymostafa1 <62448785+alymostafa1@users.noreply.github.com>
2024-08-05 01:48:33 +0300

    Finalized prisma configuration file with migration to db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import express from "express";
+import { PrismaClient } from "@prisma/client";
 
-const app = express();
-const port = 3000;
+const prisma = new PrismaClient();
 
-app.get("/", (req, res) => {
-  res.send("Hello, Khalouda!");
-});
+async function main() {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username: "new_user",
+        password: "securepassword",
+        email: "new.user@example.com1",
+        first_name: "New User",
+        last_name: "New User",
+        phone: "123-456-7890",
+        isEmployee: true,
+        role_id: 1,
+      },
+    });
+    console.log(newUser);
+  } catch (e) {
+    console.log("Error:", e);
+  }
+}
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
